a61b8c30a3987ea33c748f880e8ad99a08d92ee1
Natasha Gallant <natashagallant18@gmail.com>
2026-02-23 20:26:23 -0500

    big commit: Collect and collectStrings main emit structure lots of helpers type mapping

--- irgen/types.go
+++ irgen/types.go
@@ -2,17 +2,18 @@ package irgen
 
 import (
 	"bytes"
+	"sydney/types"
 )
 
 type BasicIrType string
 
 const (
-	IrInt    BasicIrType = "i64"
-	IrFloat  BasicIrType = "double"
-	IrBool   BasicIrType = "i1"
-	IrString BasicIrType = "ptr"
-	IrNull   BasicIrType = "ptr" // nullptr
-	IrUnit   BasicIrType = "void"
+	IrInt   BasicIrType = "i64"
+	IrFloat BasicIrType = "double"
+	IrBool  BasicIrType = "i1"
+	IrPtr   BasicIrType = "ptr"
+	IrNull  BasicIrType = "ptr" // nullptr
+	IrUnit  BasicIrType = "void"
 )
 
 type IrStruct struct {
@@ -24,7 +25,7 @@ type IrType interface {
 	String() string
 }
 
-func (s *IrStruct) String() {
+func (s *IrStruct) String() string {
 	var out bytes.Buffer
 	out.WriteString("%struct.")
 	out.WriteString(s.Name)
@@ -36,8 +37,37 @@ func (s *IrStruct) String() {
 		}
 	}
 	out.WriteString(" }")
+
+	return out.String()
 }
 
 func (b BasicIrType) String() string {
 	return string(b)
+}
+
+func SydneyTypeToIrType(t types.Type) IrType {
+	switch t {
+	case types.Int:
+		return IrInt
+	case types.Float:
+		return IrFloat
+	case types.Bool:
+		return IrBool
+	case types.String:
+		return IrPtr
+	case types.Null:
+		return IrNull
+	case types.Unit:
+		return IrUnit
+	}
+
+	switch t.(type) {
+	case types.StructType:
+		return IrPtr // ptr — structs passed by pointer
+	case types.FunctionType:
+		return IrPtr // ptr — function pointer
+	case types.InterfaceType:
+		return IrPtr // ptr — interface fat pointer
+	}
+	return IrUnit
 }
