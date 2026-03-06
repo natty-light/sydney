117b408e252e6e19b7d9f8d0479598614f46b902
Natasha Gallant <natashagallant18@gmail.com>
2026-03-06 11:20:54 -0500

    map gc support

--- sydney_rt/src/map.rs
+++ sydney_rt/src/map.rs
@@ -1,9 +1,16 @@
 use std::collections::HashMap;
 use std::ffi::CStr;
+use std::path::Component::ParentDir;
+use crate::gc::GC;
 
 #[no_mangle]
 pub extern "C" fn sydney_map_create_int() -> *mut HashMap<i64, i64> {
-  Box::into_raw(Box::new(HashMap::new()))
+  let map = Box::into_raw(Box::new(HashMap::new()));
+  unsafe {
+    let gc = GC.as_mut().unwrap();
+    gc.maps.push(map)
+  }
+  map
 }
 #[no_mangle]
 pub extern "C" fn sydney_map_set_int(map: *mut HashMap<i64, i64>, key: i64, value: i64) {
@@ -15,7 +22,12 @@ pub extern "C" fn sydney_map_get_int(map: *const HashMap<i64, i64>, key: i64) ->
 }
 #[no_mangle]
 pub extern "C" fn sydney_map_create_string() -> *mut HashMap<String, i64> {
-  Box::into_raw(Box::new(HashMap::new()))
+  let map = Box::into_raw(Box::new(HashMap::new()));
+  unsafe {
+    let gc = GC.as_mut().unwrap();
+    gc.maps.push(map)
+  }
+  map
 }
 #[no_mangle]
 pub extern "C" fn sydney_map_set_str(map: *mut HashMap<String, i64>, key: *const u8, value: i64) {
@@ -30,5 +42,23 @@ pub extern "C" fn sydney_map_get_str(map: *const HashMap<String, i64>, key: *con
   unsafe {
     let k = CStr::from_ptr(key as *const i8).to_string_lossy();
     *(*map).get(k.as_ref()).unwrap_or(&0)
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn sydney_map_get_destroy_int(map: *mut HashMap<i64, i64>) {
+  unsafe {
+    let gc = GC.as_mut().unwrap();
+    gc.maps.retain(|m| (*m as *const () as usize) != (map as *const () as usize));
+    drop(Box::from_raw(map));
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn sydney_map_get_destroy_string(map: *mut HashMap<String, i64>) {
+  unsafe {
+    let gc = GC.as_mut().unwrap();
+    gc.maps.retain(|m| (*m as *const () as usize != (map as *const () as usize)));
+    drop(Box::from_raw(map));
   }
 }
